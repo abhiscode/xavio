31a7e798788ebdf1bb5028ef7031670a3ca60514
abhiscode <abhisheksingh972193@gmail.com>
2019-11-27 15:33:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* global fetch */
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import logo from 'https://www.xavio.in/img/logoBS.png';
 import './App.css';
 
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://www.xavio.in/img/logoBS.png"} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>XAVIO</p>
         <p>Freelance for those who don''t know freelance</p>
         <p>
